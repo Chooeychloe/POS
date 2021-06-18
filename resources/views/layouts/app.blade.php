0941bf8a5897382bc5b49bf9b5eef62ebd6febae
paulsong213 <songaliajohnpaul@gmail.com>
2021-06-18 19:07:09 +0800

    Design Receipt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @livewireStyles
+    @livewireStyles()
 </head>
 
 <body>
@@ -148,7 +148,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
         integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous">
     </script>
-    @livewireScripts
+    @livewireScripts()
 </body>
 
 @yield('script')
--- resources/views/reports/receipt.blade.php
+++ resources/views/reports/receipt.blade.php
@@ -1,24 +1,99 @@
 <div id="invoice-POS">
-    <div id="printed_content">
-
-        <center id="logo">
-            <div class="logo"> PEPAY's</div>
-            <div class="info"></div>
-            <h2>Pepay's Sexual Desires Ltd</h2>
-        </center>
-    </div>
+    <?php $total = 0;
+        $tax = 0;
+    ?>
+    <nav id="receipt-nav">
+        <div class="name-cotainer">
+            <h1 class="company-name">STONE BASE</h1>
+            
+            <div class="info-email-number">
+                <h6 class="email">stonebase@gmail.com</h6>
+                <h6>+63912346754</h6>
+            </div>
+        </div>
+        <div class="logo-container">
+            <img class="logo" src="/images/logo.png" alt="logo">
+        </div>
+    </nav>
     
-    <div class="mid test-class">
-        <div class="info">
-            <h2>Contact Us</h2>
-            <p>
-                Address : Bacoor, Cavite
-                Email : fey@gmail.com
-                Phone : +639 99 9999 999
+    <section class="heading">
+
+        <div class="receipt-info">
+            <div>
+                <h6>
+                    Serial #: 
+                </h6>
+                <h6>
+                    Date:
+                </h6>
+                <h6>
+                    Address:
+                </h6>
+            </div>
+            <div>
+                <h6>1234</h6>
+                <h6>20-01-2001</h6>
+                <h6>John Doe</h6>
+            </div>
+        </div>
+        <div>
+            <h1 class="title">RECEIPT</h1>
+        </div>
+    </section>
+
+    <main>
+        <div class="t-head">
+            <h3>Item</h3>
+            <h3>Quantity</h3>
+            <h3>Unit</h3>
+            <h3>Discount</h3>
+            <h3>Subtotal</h3>
+        </div>
+
+        <div class="t-body">
+            {{-- ROW --}}
+            @foreach ($order_receipt as $receipt)
+            <div class="t-row">
+                <h3>{{$receipt -> product-> product_name}}</h3>
+                <h3>{{$receipt -> quantity}}</h3>
+                <h3>{{number_format( $receipt -> unitprice, 2)}}</h3>
+                <h3>{{$receipt -> discount ? ' ': '0'}}</h3>
+                <h3>{{number_format( $receipt -> amount, 2)}}</h3>
+            </div>
+
+            @php
+            
+            @endphp
+
+            <?php $total += $receipt->amount ?>
+
+            @endforeach
+
+        </div>
+
+        {{-- Tax --}}
+        <div class="tax-price">
+            <h2>Tax</h2>
+            <h2>₱100</h2>
+        </div>
+        {{-- Total price --}}
+        <div class="total-price">
+            <h2>₱<?= $total ?></h2>
+        </div>
+    </main>
+
+    <footer>
+        <div class="legalcopy">
+            <p class="legal">
+                <strong>** Thank you for visiting **
+                <br>
+                The goods are subject to taxes. Prices includes tax.
+            </strong>
             </p>
         </div>
-    </div>
-    <div class="bot">
+    </footer>
+
+    <div class="bot" style="display: none">
         <div id="table">
             <table>
                 <tr class="tabletitle">
@@ -77,106 +152,197 @@
 </div>
 
 <style>
+
+    *{
+        font-family: Nunito, Arial, Helvetica, sans-serif;
+        margin: 0;
+    }
+
+    #receipt-nav{
+        background: url('/images/receipt-nav-bg.png');
+        background-repeat: no-repeat;
+        background-size: 80%;
+        max-height: 15mm;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .logo{
+        width: 15mm;
+        height: auto;
+        margin: auto;
+    }
+
+    .logo-container{
+        display: flex;
+        justify-content: center;
+        width: 22mm;
+    }
+
+    .name-cotainer{
+        display: flex;
+        justify-content: center;
+        position: relative;
+        flex-direction: column;
+    }
+
+    .company-name{
+        font-size: 20px;
+        color: #fff;
+        text-align: center;
+        margin: auto 10px;
+    }
+
+    .infos{
+        position: absolute;
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        height: 20px;
+        padding: 0 10px;
+    }
+
+    .infos h6 {
+        color: rgba(255,255,255,0.7);
+        font-size: 2mm;
+    }
+
+    .info-email-number{
+        display: flex;
+        justify-content: space-between;
+        padding-left: 10px;
+    }
+
+    .info-email-number h6{
+        color: rgba(255,255,255,0.7);
+        font-size: 2mm;
+    }
+
+    .heading {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 10px;
+    }
+
+    .heading .title {
+        color: #1482C0;
+        font-size: 20px;
+        margin: auto 0;
+    }
+
+    .heading h6 {
+        color: rgba(0,0,0,0.7);
+        font-size: 6px;
+    }
+
+    .receipt-info {
+        display: flex;
+    }
+
+    .receipt-info div {
+        margin: 0 3px;
+    }
+
     #invoice-POS{
         box-shadow: 0 0 1in -0.25in rgb(0, 0, 0.5);
-        padding: 2mm;
         margin: 0 auto;
         width: 70mm;
         background: #fff;
-
+        padding-bottom: 20px;
     }
-    #invoice-POS ::selection{
-        background: #34495E;
-        color: #fff;
-    }
-
-    #invoice-POS ::-moz-selection{
-        background: #34495E;
-        color: #fff;
-    }
-    #invoice-POS h1{
-        font-size: 1.5em;
-        columns: #222;
-    }
-    #invoice-POS h2{
-        font-size: 0.5xem;
     
-    }
-    #invoice-POS h3{
-        font-size: 1.2em;
-        font-weight: 300;
-        line-height: 2em;
-    }
-    #invoice-POS p{
-        font-size: 0.7em;
-        color: #666;
-        line-height: 1.2em;
-    }
-    #invoice-POS #top, #invoice-POS #mid, #invoice-POS #bot{
-        border-bottom: 1px solid #eee;
-    
-    }
-    #invoice-POS #top{
-        min-height: 100px;
-
-    }
-    #invoice-POS #mid{
-        min-height: 80px;
-
-    }
-    #invoice-POS #bot{
-        min-height: 50px;
-
-    }
-    #invoice-POS #top .logo{
-        height: 60px;
-        width: 60px;
-        background-image: url() no-repeat;
-        background-size: 60px 60px;
-        border-radius: 50px ;
-    }
-    #invoice-POS .info {
-       display: block;
-       margin-left: 0;
-       text-align: center;
-    }
-    #invoice-POS .title{
-       float: right;
-    }
-    #invoice-POS .title p{
-       text-align: right;
-    }
-    #invoice-POS table{
-       width: 100%;
-       border-collapse: collapse;
+    .t-head {
+        background: url('/images/receipt-thead-bg.jpg');
+        background-size: 100%;
+        background-repeat: no-repeat;
+        padding: 10px 10px;
+        display: flex;
+        justify-content: space-evenly;
     }
 
-    #invoice-POS .tabletitle{
-        font-size: 0.5em;
-        background: #eee;
-    }
-    #invoice-POS .service{
-        border-bottom: 1px solid #eee;
-    }
-    #invoice-POS .item{
-        width: 24mm;
-    }
-    #invoice-POS .itemtext{
-        font-size: 0.5em;
-    }
-
-    #invoice-POS #legalcopy{
-        margin-top: 5mm ;
+    .t-head h3{
+        font-size: 8px;
+        color: #fff;
         text-align: center;
+        width: 100%;
     }
-    .serial-number{
-        margin-top: 5mm;
-        margin-bottom: 2mm;
+
+    .t-head h3:nth-child(1){
+        margin-right: 50px;
+    }
+
+    .t-row h3:nth-child(1){
+        margin-right: 50px;
+    }
+
+    .t-body {
+        display: flex;
+        flex-direction: column;
+        padding: 5px 0;
+    }
+
+    .t-row {
+        padding: 10px 10px;
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .t-row h3{
+        font-size: 8px;
+        color: rgb(38, 37, 37);
         text-align: center;
+        width: 100%;
+    }
+
+    .t-row:nth-child(even) {
+       background-color: rgb(243,243,243);
+    }
+
+    .total-price {
+        background: url('/images/receipt-total-bg.jpg');
+        background-size: 100%;
+        background-repeat: no-repeat;
+        width: 30mm;
+        margin-top: 5px;
+        margin-left: auto;
+        margin-right: 5px;
+        margin-bottom: 10px;
+        display: flex;
+        justify-content: end;
+        padding: 5px 5px;
+    }
+
+    .total-price h2 {
+        color: #fff;
         font-size: 12px;
+        width: 100%;
+        text-align: right;
     }
-    .serial{
-        font-size: 10px !important;
+
+    .legalcopy {
+        font-size: 8px;
+        color: rgba(0,0,0,0.5);
+        text-align: center;
+        margin-top: 10px;
+        margin-bottom: 5px; 
+    }
+
+    .tax-price {
+        width: 30mm;
+        margin-top: 5px;
+        margin-left: auto;
+        margin-right: 5px;
+        margin-bottom: 5px;
+        display: flex;
+        justify-content: end;
+        padding: 5px 5px;
+    }
+
+    .tax-price h2 {
+        color: rgba(0,0,0,0.6);
+        font-size: 10px;
+        width: 100%;
+        text-align: right;
     }
 
 
